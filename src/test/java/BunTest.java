e2eaf99ca39eccf598c6f4c47e3e76d77512d4f0
psasha007 <psasha007@yandex.ru>
2022-11-25 22:42:00 +0300

    Добавил тесты для проверки методов класса Bun equalGetNameBunTest - Проверка метода названия Булочки equalGetPriceBunTest - Проверка метода назначенной цены Булочки
    Попробовал добавить jacoco

--- src/test/java/BunTest.java
+++ src/test/java/BunTest.java
@@ -1,3 +1,33 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import praktikum.Bun;
+
 public class BunTest {
 
+    private Bun bun = new Bun("NameBun",100.00f);
+    private String nameBun = "NameBun";
+    private float priceBun = 100.00f;
+
+    @Before
+    public void createBun() {
+        bun = new Bun(nameBun, priceBun);
+    }
+
+    // Проверка метода названия Булочки
+    @Test
+    public void equalGetNameBunTest() throws Exception {
+        String expected = "NameBun";
+        String actual = bun.getName();
+        Assert.assertEquals(expected, actual);
+    }
+
+    // Проверка метода назначенной цены Булочки
+    @Test
+    public void equalGetPriceBunTest() throws Exception {
+        float expected = 100.00f;
+        float actual = bun.getPrice();
+        float delta = 0.00f;
+        Assert.assertEquals(expected, actual, delta);
+    }
 }
